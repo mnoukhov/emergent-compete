human fixed, policy gradient weight decay
deterministic analysis done, it's not great
policy gradient working to diff ~2-3
bias sampling now done correctly

src/agents.py
@@ -13,7 +13,7 @@ from utils import discount_return
 
 
 class Policy(nn.Module):
-    def __init__(self, mode, gamma=1.0):
+    def __init__(self, mode, gamma=1.0, **kwargs):
         super().__init__()
         self.mode = mode
         self.gamma = gamma
@@ -45,16 +45,16 @@ class Policy(nn.Module):
 @gin.configurable
 class Human(Policy):
     def action(self, state):
-        obs, _, _ = state
+        obs = state[0]
 
         if self.mode == 0:
             prompt = 'target {}: '.format(obs.item())
         elif self.mode == 1:
             prompt = 'message {}: '.format(obs.item())
 
-        action = int(input(prompt))
+        action = float(input(prompt))
 
-        return torch.tensor(action)
+        return torch.tensor([action])
 
 
 @gin.configurable
@@ -142,64 +142,57 @@ class DeterministicGradient(Policy):
     def __init__(self, mode, num_actions, lr):
         super().__init__(mode)
         self.num_actions = num_actions
-        self.policy = nn.Linear(3,1)
+        self.policy = nn.Linear(4,1)
         init.uniform_(self.policy.weight, 1/9, 1/3)
-        init.uniform_(self.policy.bias, 1/9, 1/3)
+        init.uniform_(self.policy.bias, 0, 1)
         self.optimizer = Adam(self.policy.parameters(), lr=lr)
+        print(self.policy.weight)
+        print(self.policy.bias)
 
         self.logger.update({
             'loss': [],
             '20': [],
-            'preact grad': [],
-            'act grad': []
         })
-        self.grad = []
+        self.weight_grad = []
         self.act_grad = []
         self.preact_grad = []
-        self.policy.weight.register_hook(self.grad.append)
+        self.policy.weight.register_hook(self.weight_grad.append)
 
     def reset(self):
         super().reset()
-        del self.grad[:]
+        del self.weight_grad[:]
         del self.act_grad[:]
         del self.preact_grad[:]
 
     def action(self, state):
         input_ = torch.cat(state, dim=1)
         action = self.policy(input_)
-        action.register_hook(self.preact_grad.append)
-        clamped = torch.clamp(action, 0, self.num_actions)
-        clamped.register_hook(self.act_grad.append)
-
-        return clamped
+        return action
 
     def update(self):
         super().update()
         loss = -torch.stack(self.rewards).mean()
         self.logger['loss'].append(loss.item())
+        out20 = self.policy(torch.tensor([20., 0., 0., 0.]))[0].item()
+        self.logger['20'].append(out20)
 
         self.optimizer.zero_grad()
         loss.backward()
         self.optimizer.step()
 
-        self.logger['act grad'].append(torch.stack(self.act_grad).norm(2).item())
-        self.logger['preact grad'].append(torch.stack(self.preact_grad).norm(2).item())
-        self.logger['20'].append(self.policy(torch.tensor([20., 0., 0.]))[0].item())
-        # self.logger['grad'].append(torch.stack(self.grad).norm(2).item())
-
 
 @gin.configurable
 class PolicyGradient(Policy):
-    def __init__(self, num_actions, lr, gamma, ent_reg, min_std, mode):
+    def __init__(self, num_actions, lr, weight_decay, gamma, ent_reg, min_std, mode):
         super().__init__(mode)
         self.num_actions = num_actions
         self.gamma = gamma
         self.min_std = torch.tensor(min_std)
-        self.policy = nn.Linear(3,2)
-        init.uniform_(self.policy.weight, 1/9, 1)
-        init.uniform_(self.policy.bias, 1/9, 1)
+        self.policy = nn.Linear(4,2)
+        # init.uniform_(self.policy.weight, 1/9, 1)
+        # init.uniform_(self.policy.bias, 1/9, 1)
 
-        self.optimizer = Adam(self.parameters(), lr=lr)
+        self.optimizer = Adam(self.parameters(), lr=lr, weight_decay=weight_decay)
         self.ent_reg = ent_reg
 
         self.logger.update({
@@ -237,7 +230,7 @@ class PolicyGradient(Policy):
 
     def update(self):
         super().update()
-        out20 = self.policy(torch.tensor([20., 0., 0.]))[0]
+        out20 = self.policy(torch.tensor([20., 0., 0., 0.]))[0]
 
         returns = torch.cat(discount_return(self.rewards, self.gamma), dim=1)
         logprobs = torch.cat(self.log_probs, dim=1)

src/game.py
@@ -46,12 +46,11 @@ class IteratedSenderRecver(gym.Env):
         self.bias_space = DiscreteRange(min_bias, max_bias)
         self.batch_size = batch_size
 
-        self.biases = []
         self.send_diffs = []
         self.recv_diffs = []
 
     def _generate_bias(self):
-        return torch.randint(self.bias_space.low, self.bias_space.high,
+        return torch.randint(self.bias_space.low, self.bias_space.high + 1,
                              size=(self.batch_size,1)).float()
 
     def _generate_target(self):
@@ -60,6 +59,13 @@ class IteratedSenderRecver(gym.Env):
 
 
     def _reward(self, pred, target=None):
+        if target is None:
+            target = torch.tensor(0.)
+        diff = torch.abs(pred - target)
+        dist = torch.min(diff, self.num_targets - diff)
+        return 1 - 2 * dist / self.num_targets
+
+    def _cos_reward(self, pred, target=None):
         if target is None:
             target = torch.tensor(0.)
         norm_dist = (pred - target) / self.num_targets
@@ -72,8 +78,6 @@ class IteratedSenderRecver(gym.Env):
         self.recv_target = self._generate_target()
         self.send_target = (self.recv_target + self.bias) % self.num_targets
 
-        self.biases.append(self.bias.mean())
-
         return self.send_target
 
     def step(self, action):
@@ -83,11 +87,12 @@ class IteratedSenderRecver(gym.Env):
                    self._reward(action, self.recv_target)]
         done = (self.round >= self.num_rounds)
 
+        action = action % self.num_targets
+        send_diffs = torch.abs(action - self.send_target)
+        send_diffs = torch.min(self.num_targets - send_diffs, send_diffs)
+        recv_diffs = torch.abs(action - self.recv_target)
+        recv_diffs = torch.min(self.num_targets - recv_diffs, recv_diffs)
         if done:
-            send_diffs = torch.abs(action - self.send_target)
-            send_diffs = torch.min(self.num_targets - send_diffs, send_diffs)
-            recv_diffs = torch.abs(action - self.recv_target)
-            recv_diffs = torch.min(self.num_targets - recv_diffs, recv_diffs)
             self.send_diffs.append(send_diffs.mean().item())
             self.recv_diffs.append(recv_diffs.mean().item())
 
@@ -103,7 +108,7 @@ class IteratedSenderRecver(gym.Env):
         self.recv_target = self._generate_target()
         self.send_target = (self.recv_target + self.bias) % self.num_targets
 
-        return self.send_target, rewards, done
+        return self.send_target, rewards, done, [send_diffs, recv_diffs]
 
     def render(self, message=-1.0):
         print('--- round {} ---'.format(self.round_info['round']))

src/train.py
@@ -23,19 +23,23 @@ def train(Sender, Recver, env, episodes, render, log, savedir):
         prev_target = torch.zeros(env.batch_size, 1)
         prev_message = torch.zeros(env.batch_size, 1)
         prev_guess = torch.zeros(env.batch_size, 1)
+        prev_send_reward = torch.zeros(env.batch_size, 1)
+        prev_recv_reward = torch.zeros(env.batch_size, 1)
 
         done = False
         while not done:
-            message = sender.action([target, prev_target, prev_message])
-            guess = recver.action([message, prev_message, prev_guess])
+            message = sender.action([target, prev_target, prev_message, prev_send_reward])
+            guess = recver.action([message, prev_message, prev_guess, prev_recv_reward])
 
-            target, rewards, done = env.step(guess)
+            target, rewards, done, diffs = env.step(guess)
             prev_target = target.detach()
             prev_message = message.detach()
             prev_guess = guess.detach()
 
             sender.rewards.append(rewards[0])
             recver.rewards.append(rewards[1])
+            prev_send_reward = diffs[0].detach()
+            prev_recv_reward = diffs[1].detach()
 
             if render and e % render == 0:
                 env.render(message=message[0].item())
@@ -48,8 +52,6 @@ def train(Sender, Recver, env, episodes, render, log, savedir):
             print('REWD    {:2.2f}     {:2.2f}'.format(sender.last('ep_reward'), recver.last('ep_reward')))
             print('LOSS    {:2.2f}     {:2.2f}'.format(sender.last('loss'), recver.last('loss')))
             print('DIFF    {:2.2f}     {:2.2f}'.format(env.send_diffs[-1], env.recv_diffs[-1]))
-            # print('PGRADS  {:2.4f}     {:2.4f}'.format(sender.last('preact grad'), recver.last('preact grad')))
-            # print('AGRADS  {:2.4f}     {:2.4f}'.format(sender.last('act grad'), recver.last('act grad')))
             print('')
 
     print('Game Over')
@@ -70,30 +72,28 @@ def plot_and_save(x, sender, recver, env, savedir):
     nocomm_rew = env._reward(nocomm_loss)
     midbias_loss = env.bias_space.low + (env.bias_space.range / 2)
     midbias_rew = env._reward(midbias_loss)
-    truebias_rew = env._reward(torch.stack(env.biases)).numpy()
     plt.plot(x, running_mean(avg_rew, 100), label='avg reward')
     plt.plot(x, running_mean(srew, 100), label='sender')
     plt.plot(x, running_mean(rrew, 100), label='recver')
     plt.axhline(nocomm_rew, label='nocomm baseline')
     plt.axhline(midbias_rew, label='midbias baseline')
-    plt.plot(x, running_mean(truebias_rew), label='truebias baseline')
     plt.legend()
     if savedir:
         plt.savefig(f'{savedir}/rewards.png')
     plt.show()
 
     # REWARD PER ROUND
-    sround = np.array(sender.logger['round_reward'])
-    rround = np.array(recver.logger['round_reward'])
-    avg_round = (sround + rround) / 2
-    for r in range(env.num_rounds):
-        # plt.plot(x, running_mean(avg_round[:,r]), label='avg_round-{}'.format(r))
-        # plt.plot(x, running_mean(sround[:,r]), label='sender-{}'.format(r))
-        plt.plot(x, running_mean(rround[:,r]), label='recver-{}'.format(r))
-    plt.legend()
-    if savedir:
-        plt.savefig(f'{savedir}/round.png')
-    plt.show()
+    # sround = np.array(sender.logger['round_reward'])
+    # rround = np.array(recver.logger['round_reward'])
+    # avg_round = (sround + rround) / 2
+    # for r in range(env.num_rounds):
+        # # plt.plot(x, running_mean(avg_round[:,r]), label='avg_round-{}'.format(r))
+        # # plt.plot(x, running_mean(sround[:,r]), label='sender-{}'.format(r))
+        # plt.plot(x, running_mean(rround[:,r]), label='recver-{}'.format(r))
+    # plt.legend()
+    # if savedir:
+        # plt.savefig(f'{savedir}/round.png')
+    # plt.show()
 
     # ABS DIFF AT ROUND 5
     plt.plot(x, running_mean(env.send_diffs), label='sender')
@@ -104,14 +104,23 @@ def plot_and_save(x, sender, recver, env, savedir):
         plt.savefig(f'{savedir}/diff.png')
     plt.show()
 
+    # GRADS
+    # plt.plot(x, running_mean(sender.logger['preact grad']), label='sender-grad')
+    plt.plot(x, running_mean(recver.logger['preact grad']), label='recver-norm')
+    # plt.plot(x, running_mean(sender.logger['grad var']), label='sender-var')
+    plt.plot(x, running_mean(recver.logger['grad var']), label='recver-var')
+    plt.plot(x, running_mean(recver.logger['grad mean']), label='recver-mean')
+    plt.legend()
+    plt.show()
+
     # OUTPUT FOR 20
     # plt.plot(x, sender.logger['20'], label='sender')
-    plt.plot(x, recver.logger['20'], label='recver')
-    plt.title('Output for Input=20')
-    plt.legend()
-    if savedir:
-        plt.savefig(f'{savedir}/20.png')
-    plt.show()
+    # plt.plot(x, recver.logger['20'], label='recver')
+    # plt.title('Output for Input=20')
+    # plt.legend()
+    # if savedir:
+        # plt.savefig(f'{savedir}/20.png')
+    # plt.show()
 
     # ENTROPY
     # plt.plot(x, recver.logger['entropy'], label='entropy')