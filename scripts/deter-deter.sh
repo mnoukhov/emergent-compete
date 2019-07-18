af22c4903d606b98369468220604d59389bba761
Michael Noukhovitch <noukhovm@mila.quebec>
2019-07-18 01:42:21 -0400

    fixed time

--- scripts/deter-deter.sh
+++ scripts/deter-deter.sh
@@ -3,14 +3,14 @@
 #SBATCH --output=job_output.txt
 #SBATCH --error=job_error.txt
 #SBATCH --ntasks=1
-#SBATCH --time=1:00
+#SBATCH --time=5:00
 #SBATCH --mem=4Gb
 
 export PYTHONPATH='/network/home/noukhovm/emergent-selfish'
 source activate selfish
 
 bias=9
-vocab=3
+vocab=2
 savedir="deter-deter-bias${bias}-vocab${vocab}/${SLURM_ARRAY_TASK_ID}"
 
 python src/train.py -p train.savedir="'${savedir}'" ISR.min_bias=${bias} train.vocab_size=${vocab} -f deter-deter.gin
