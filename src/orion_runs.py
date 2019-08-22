d4ab223b5924b22b185173c820f57d631125e99e
Michael Noukhovitch <noukhovm@mila.quebec>
2019-08-22 12:42:18 -0400

    random seed as hyperparam via max of seeds

--- src/orion_runs.py
+++ src/orion_runs.py
@@ -13,6 +13,7 @@ if __name__ == '__main__':
     parser.add_argument('--config', required=True, nargs='+')
     parser.add_argument('--gin_param', '-p', nargs='+')
     parser.add_argument('--savedir')
+    parser.add_argument('--objective', choices=['mean', 'max'], default='max')
     args = parser.parse_args()
 
     # change device to torch.device
@@ -21,9 +22,7 @@ if __name__ == '__main__':
 
     gin.parse_config_files_and_bindings(args.config, args.gin_param)
 
-
-    mean_best_reward = 0
-
+    rewards = []
     for random_seed in range(5):
         if args.savedir:
             os.makedirs(args.savedir, exist_ok=True)
@@ -34,12 +33,18 @@ if __name__ == '__main__':
 
         best_reward = train(savedir=seed_savedir,
                             random_seed=random_seed)
-        mean_best_reward += best_reward
+        rewards.append(best_reward)
 
-    mean_best_reward /= 5
+    if args.objective == 'mean':
+        objective = -sum(rewards) / len(rewards)
+    elif args.objective == 'max':
+        objective = -max(rewards)
+    else:
+        raise NotImplementedError(f'no objective: {args.objective}')
 
-    print(f'mean best reward over seeds {mean_best_reward:2.2f}')
+    print(f'{args.objective} best objective over seeds {objective:2.2f}')
+
     report_results([dict(
         name='best_neg_reward',
         type='objective',
-        value=-mean_best_reward)])
+        value=objective)])
