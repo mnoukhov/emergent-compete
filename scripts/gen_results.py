3c5fa2de90eca364736daff3443523aa11607b3f
Michael Noukhovitch <mnoukhov@gmail.com>
2019-09-20 00:49:07 -0400

    error metric l2

--- scripts/gen_results.py
+++ scripts/gen_results.py
@@ -6,7 +6,7 @@ import shutil
 import pandas as pd
 
 
-def metric(seeds_dir, verbose=False):
+def metric(seeds_dir, error_name='l1', verbose=False):
     # average of last 10 epochs
     results_path = Path(seeds_dir)
 
@@ -30,12 +30,20 @@ def metric(seeds_dir, verbose=False):
     epoch = logs['epoch']
     sender = pd.DataFrame(logs['sender'].tolist()).join(logs['epoch'])
     recver = pd.DataFrame(logs['recver'].tolist()).join(logs['epoch'])
-    error_metric = 'test_l1_error' if 'test_l1_error' in sender else 'test_error'
+    if error_name == 'l1' and 'test_l1_error' in sender:
+        error_metric = 'test_l1_error'
+    if error_name == 'l2' and 'test_l2_error' in sender:
+        error_metric = 'test_l2_error'
+    elif error_name == 'train':
+        error_metric = 'test_error'
+    else:
+        raise Exception(f'error name {error_name} either not found or not valid')
+
     total_error = sender[error_metric] + recver[error_metric]
     return total_error.to_frame().groupby(epoch).mean()[-10:].mean()[error_metric]
 
 
-def metric_over_runs(all_results_dir, verbose=True):
+def metric_over_runs(all_results_dir, error_name, verbose=True):
     all_results_path = Path(all_results_dir)
 
     empty =  []
@@ -45,7 +53,7 @@ def metric_over_runs(all_results_dir, verbose=True):
     for result_dir in all_results_path.iterdir():
         if result_dir.is_dir():
             try:
-                score = metric(result_dir, verbose)
+                score = metric(result_dir, error_name, verbose)
             except ValueError:
                 errors.append(result_dir.name)
             else:
@@ -62,7 +70,7 @@ def metric_over_runs(all_results_dir, verbose=True):
     return min_score, min_index
 
 
-def generate_results_csv(experiment_name, cluster_dir, output_dir='.'):
+def generate_results_csv(experiment_name, cluster_dir, output_dir='.', error_name='l1'):
     output_path = Path(output_dir)
     cluster_results_path = Path(cluster_dir)
     if not cluster_results_path.exists():
@@ -73,12 +81,12 @@ def generate_results_csv(experiment_name, cluster_dir, output_dir='.'):
     ids = []
     run_paths = []
 
-    for exp_results_path in cluster_results_path.glob(f'{experiment_name}-*'):
+    for exp_results_path in cluster_results_path.glob(f'{experiment_name}-bias*'):
         exp_full_name = exp_results_path.name
         bias_index = exp_full_name.find('bias') + 4
         name_index = len(exp_full_name) + 1
         print(f'running on {exp_full_name}')
-        error, run_name = metric_over_runs(exp_results_path, verbose=False)
+        error, run_name = metric_over_runs(exp_results_path, error_name=error_name, verbose=False)
         if error is None:
             print(f'no results in {exp_full_name}')
         else:
@@ -99,13 +107,13 @@ def generate_results_csv(experiment_name, cluster_dir, output_dir='.'):
     return run_paths
 
 
-def generate_results_folder(experiment_name, cluster_dir, output_dir):
+def generate_results_folder(experiment_name, cluster_dir, output_dir, error_name):
     output_path = Path(output_dir)
     results_folder_path = output_path / experiment_name
     results_folder_path.mkdir(exist_ok=True)
 
     print('generating results csv')
-    best_run_paths = generate_results_csv(experiment_name, cluster_dir, results_folder_path)
+    best_run_paths = generate_results_csv(experiment_name, cluster_dir, results_folder_path, error_name)
 
     print('copying files')
     for run_path in best_run_paths:
@@ -122,17 +130,20 @@ if __name__ == '__main__':
     # generate results folder
     gen_parser = subparsers.add_parser('generate')
     gen_parser.set_defaults(command='generate')
-    gen_parser.add_argument('--experiment-name', required=True)
-    gen_parser.add_argument('--results-dir', required=True)
-    gen_parser.add_argument('--output-dir', required=True)
+    gen_parser.add_argument('--experiment-name', '--exp-name', required=True)
+    gen_parser.add_argument('--results-dir', default='/scratch/noukhovm/emergent-selfish')
+    gen_parser.add_argument('--output-dir', default='/home/noukhovm/emergent-selfish/results/')
+    gen_parser.add_argument('--error', default='l1')
 
+    # get error metric over hyperparams
     check_parser = subparsers.add_parser('check')
     check_parser.set_defaults(command='check')
     check_parser.add_argument('dir')
+    check_parser.add_argument('--error', default='l1')
 
     args = parser.parse_args()
 
     if args.command == 'generate':
-        generate_results_folder(args.experiment_name, args.results_dir, args.output_dir)
+        generate_results_folder(args.experiment_name, args.results_dir, args.output_dir, args.error)
     elif args.command == 'check':
-        print(metric_over_runs(args.dir))
+        print(metric_over_runs(args.dir, args.error))
