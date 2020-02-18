c559db894fae27b151503573985fc91cb7397160
Michael <michael.noukhovitch@elementai.com>
2020-02-18 18:02:07 -0500

    anonymize and remove old scripts

--- scripts/sbatch_reruns.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-for BIAS in 0 3 6 9 12 15
-do
-    sbatch scripts/rerun_one_slurm.sh $BIAS
-done
--- setup.py
+++ setup.py
@@ -13,11 +13,8 @@ with open('notebook_requirements.txt') as f:
             notebook_reqs.append(line)
 
 setup(
-    name = 'emergent-selfish',
+    name = 'emergent-compete',
     version = '0.0.1',
-    url = 'https://github.com/mnoukhov/emergent-selfish',
-    author = 'Michael Noukhovitch',
-    author_email = 'mnoukhov@gmail.com',
     packages = find_packages(),
     install_requires=requirements,
     extras_require={'notebook': notebook_reqs},
