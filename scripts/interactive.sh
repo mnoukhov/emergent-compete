6262103046db857042fdceec16b71236f3200442
Michael Noukhovitch <michael.noukhovitch@elementai.com>
2020-04-12 22:43:33 +0000

    interactive script

---
+++ scripts/interactive.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+if [ -z "$WANDB_API_KEY" ]
+then
+    echo "var WANDB_API_KEY not provided, make sure you're not running with --wandb"
+fi
+
+borgy submit -I \
+    --mem 8 \
+    -e WANDB_API_KEY=$WANDB_API_KEY \
+    -i volatile-images.borgy.elementai.net/mnoukhov/emergent:latest \
+    -v $HOME:$HOME:rw \
+    -- bash -c "while true; do sleep 60; done"
