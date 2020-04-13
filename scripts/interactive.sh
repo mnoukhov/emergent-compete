36a125e1156a27cbe7be2df19c4c6937e5cf19e7
Michael Noukhovitch <michael.noukhovitch@elementai.com>
2020-04-13 03:06:26 +0000

    interactive borgy script

--- scripts/interactive.sh
+++ scripts/interactive.sh
@@ -6,8 +6,10 @@ then
 fi
 
 borgy submit -I \
-    --mem 8 \
+    --mem 4 \
+    -e HOME=$HOME \
     -e WANDB_API_KEY=$WANDB_API_KEY \
     -i volatile-images.borgy.elementai.net/mnoukhov/emergent:latest \
     -v $HOME:$HOME:rw \
+    -v /mnt/scratch/mnoukhov/emergent:/scratch:rw \
     -- bash -c "while true; do sleep 60; done"
