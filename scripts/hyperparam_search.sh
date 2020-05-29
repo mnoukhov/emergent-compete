edded037004b65417fa454937abdff2d8be39092
Michael Noukhovitch <mnoukhov@gmail.com>
2020-05-29 18:47:39 -0400

    slurm stuff

--- scripts/hyperparam_search.sh
+++ scripts/hyperparam_search.sh
@@ -1,16 +1,34 @@
-max_trials=$1
-experiment_name=$2
-config=$3
-params=${@:4}
+#!/bin/bash
+#SBATCH --account=def-bengioy
+#SBATCH --cpus-per-task=1
+#SBATCH --output=/home/noukhovm/scratch/slurm-logs/hyperparam-search.%A.%a.out
+#SBATCH --error=/home/noukhovm/scratch/slurm-logs/hyperparam-search.%A.%a.err
+#SBATCH --job-name=ec-search
+#SBATCH --mem=4GB
+#SBATCH --time=2:59:00
+
+module load python/3.7
+module load scipy-stack
+virtualenv --no-download $SLURM_TMPDIR/env
+source $SLURM_TMPDIR/env/bin/activate
+pip install --no-index --upgrade pip
+pip install --no-index -r requirements.txt
+
+max_trials=$num_trials
+experiment_name=$name
+config=$config
+params=$params
 
 export PYTHONUNBUFFERED=1
 
 orion --debug hunt -n $experiment_name	\
-    --working-dir /tmp/$experiment_name \
+    --working-dir $SLURM_TMPDIR/$experiment_name \
     --max-trials $max_trials \
     orion_runs.py --config configs/$config \
     --savedir {trial.working_dir} \
     --gin_param $params
 
-mkdir -p /scratch/$experiment_name
-cp -r /tmp/$experiment_name/* /scratch/$experiment_name/
+mkdir -p $SCRATCH/emergent-selfish/$experiment_name
+cp -r $SLURM_TMPDIR/$experiment_name/* $SCRATCH/emergent-selfish/$experiment_name
+rm -rf $SLURM_TMPDIR/env
+
---
+++ scripts/slurm-search.sh
@@ -0,0 +1,10 @@
+
+NAME=""
+NUM_PROCS=5
+max_trials=$(( 100 / $NUM_PROCS ))
+
+
+for BIAS in 0 3 6 9 12 15
+do
+
+done
