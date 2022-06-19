5303e917e2d4a70dd220dda31b5dd86aea9bf746
Michael <michael.noukhovitch@servicenow.com>
2022-06-19 15:26:03 +0000

    hyperparam search working
    all_hyperparams config
    randomness removed from exp_configs

--- haven_config/exp_configs.py
+++ haven_config/exp_configs.py
@@ -1,9 +1,13 @@
 from collections import ChainMap
 from datetime import datetime
 
+import numpy as np
 from scipy.stats import loguniform
 
-timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+# timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+
+# fix the different random points used
+np.random.seed(0)
 
 HYPERPARAM_GROUPS = {
     f"hyperparam_bias{bias}": [
@@ -15,12 +19,20 @@ HYPERPARAM_GROUPS = {
                 f"Deterministic.lr = {loguniform.rvs(1e-4, 1e-2)}",
                 f"Reinforce.ent_reg = {loguniform.rvs(1e-4, 1)}",
             ],
-            "savedir": f"/mnt/public/results/emergent-compete/cat-deter-drift-search-bias{bias}/cat-deter-drift-bias{bias}-{i}-{timestamp}",
+            "savedir": f"/mnt/public/results/emergent-compete/cat-deter-drift-search-bias{bias}-v1/cat-deter-drift-bias{bias}-{i}",
             "loaddir": "results/cat-deter-bias0/",
         }
         for i in range(100)
     ]
     for bias in [0, 3, 6, 9, 12, 15, 18]
+}
+
+ALL_HPS = {
+    "all_hyperparams": [
+        config 
+        for hyperparam_bias_configs in HYPERPARAM_GROUPS.values()
+        for config in hyperparam_bias_configs
+    ]
 }
 
 TEST_GROUPS = {
@@ -35,4 +47,4 @@ TEST_GROUPS = {
     "test_hyperparam": HYPERPARAM_GROUPS["hyperparam_bias3"][:2],
 }
 
-EXP_GROUPS = ChainMap(HYPERPARAM_GROUPS, TEST_GROUPS)
+EXP_GROUPS = ChainMap(HYPERPARAM_GROUPS, TEST_GROUPS, ALL_HPS)
